3afdc80b72cd4b0fc5317af26caceca090724ea8
yiplee <guoyinl@gmail.com>
2020-02-23 19:38:16 +0800

    add go sdk

---
+++ client.go
@@ -0,0 +1,35 @@
+package echo
+
+import (
+	"context"
+	"errors"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const hostURL = "https://echo.yiplee.com"
+
+var client = resty.New().
+	SetHostURL(hostURL).
+	SetHeader("Content-Type", "application/json")
+
+type Payload struct {
+	MessageID   string `json:"message_id,omitempty"`
+	RecipientID string `json:"recipient_id,omitempty"`
+	Category    string `json:"category,omitempty"`
+	Data        string `json:"data,omitempty"`
+}
+
+func Send(ctx context.Context, token string, payload Payload) error {
+	req := client.R().SetContext(ctx)
+	resp, err := req.SetAuthToken(token).SetBody(payload).Post("/message")
+	if err != nil {
+		return err
+	}
+
+	if resp.IsError() {
+		return errors.New(resp.Status())
+	}
+
+	return nil
+}
